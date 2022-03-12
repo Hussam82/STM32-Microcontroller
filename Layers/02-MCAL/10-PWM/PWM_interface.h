e957c9b9f01484d37ca319d9b645188979083133
Hussam Ali <81903080+Hussam82@users.noreply.github.com>
2022-03-12 15:51:54 +0200

    Update PWM_interface.h

--- Layers/02-MCAL/10-PWM/PWM_interface.h
+++ Layers/02-MCAL/10-PWM/PWM_interface.h
@@ -8,7 +8,18 @@
 #define PWM_INTERFACE_H_
 
 #include "TIM_private.h"
-
+/* PWM supported Timers:  TIMER1    4 Channels
+                          TIMER2    4 Channels
+                          TIMER3    4 Channels
+                          TIMER4    4 Channels
+                          TIMER5    4 Channels
+                          TIMER8    4 Channels
+                          TIMER9    2 Channels 
+                          TIMER10   1 Channel
+                          TIMER11   1 Channel
+                          TIMER12   2 Channels
+                          TIMER13   1 Channel
+                          TIMER14   1 Channel */
 void MPWM_voidInit(TIM_Regs *TIMERx, uint8 copy_u8Channel);
 void MPWM_voidSetDutyCycle(TIM_Regs *TIMERx, uint8 copy_u8Channel, uint8 copy_u8DutyCycle, uint32 copy_u32PeriodInMs);
 void MPWM_voidDeInit(TIM_Regs *TIMERx, uint8 copy_u8Channel);
